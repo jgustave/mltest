30558c6752f90d0bc916e4370a47a1e3b3acc441
fattygustave <jerdavis@Jeremys-iMac.local>
2013-02-14 10:25:49 -0800

    establish project

--- src/test/java/com/jd/mltest/TestNothing.java
+++ src/test/java/com/jd/mltest/TestNothing.java
@@ -1,6 +1,8 @@
 package com.jd.mltest;
 
+import cern.colt.matrix.DoubleMatrix2D;
 import org.junit.Test;
+import cern.colt.matrix.impl.DenseDoubleMatrix2D;
 
 /**
  *
@@ -8,6 +10,9 @@ import org.junit.Test;
 public class TestNothing {
 
     @Test
-    public void testNothing() {}
+    public void testNothing() {
+        DoubleMatrix2D helloMatrix = new DenseDoubleMatrix2D(4,5);
+        helloMatrix.assign(1D);
+    }
 
 }
