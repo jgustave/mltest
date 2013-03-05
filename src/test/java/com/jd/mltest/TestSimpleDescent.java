1fd902dcab755c6c491aa6889767200555f5165f
fattygustave <jerdavis@Jeremys-iMac.local>
2013-03-04 18:15:14 -0800

    I think Logistic Works... I used test data from Coursera.ML. Lets take a moment to zero mean normalize. Will def. help the learning rate.

--- src/test/java/com/jd/mltest/TestSimpleDescent.java
+++ src/test/java/com/jd/mltest/TestSimpleDescent.java
@@ -11,6 +11,7 @@ import cern.colt.matrix.impl.DenseDoubleMatrix2D;
 
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
 
@@ -67,11 +68,9 @@ public class TestSimpleDescent {
         //initialize Independent Xi
         //Going to create test data y= .5(x1) + .33(x2)
         for( int x=0;x<NUM_EXAMPLES;x++) {
-            double x1 = random.nextDouble();
-            double x2 = random.nextDouble();
             independent.set(x, 0, 1); //We always set this to 1 for the intercept
-            independent.set(x, 1, x1);
-            independent.set(x, 2, x2 );
+            independent.set(x, 1, random.nextGaussian());
+            independent.set(x, 2, random.nextGaussian() );
         }
 
         //initialize dependent Yi
@@ -201,7 +200,6 @@ public class TestSimpleDescent {
         return( thetas );
     }
 
-
     /**
      * sum((X * theta - y) .* X(:, i)) ./ m;
      * @param alpha Learning Rate
@@ -216,9 +214,6 @@ public class TestSimpleDescent {
                                           DoubleMatrix1D dependent ) {
         Algebra algebra     = new Algebra();
 
-        // ALPHA*(1/M) in one.
-        //double  modifier    = alpha / (double)independent.rows();
-
         //hypothesis is 1/( 1+ e ^ -(theta(Transposed) * X))
         DoubleMatrix1D hypothesies = algebra.mult( independent, thetas );
 
@@ -226,7 +221,8 @@ public class TestSimpleDescent {
         hypothesies.assign(new DoubleFunction() {
             @Override
             public double apply (double val) {
-                return( logit( val ) );
+                double result = logit( val );
+                return( result );
             }
         });
 
@@ -234,32 +230,33 @@ public class TestSimpleDescent {
         //Now we have for each Xi, the difference between predicted by the hypothesis and the actual Yi
         hypothesies.assign(dependent, Functions.minus);
 
-
         //Transpose Examples(MxN) to NxM so we can matrix multiply by hypothesis Nx1
         //Note that the Transpose is constant time and doesn't create a new matrix.
         DoubleMatrix2D transposed = algebra.transpose(independent);
 
         DoubleMatrix1D deltas     = algebra.mult(transposed, hypothesies );
 
-
+        //double mod = alpha / (double)dependent.size();
         // thetas = thetas - (deltas*alpha)  in one step
         thetas.assign(deltas, Functions.minusMult(alpha));
 
-
         return( thetas );
+    }
+    public static double logit( double val ) {
+        return( 1.0 / (1.0 + Math.exp(-val)));
     }
 
     @Test
     public void testLogisticDescentMultiple() {
         //Cost function: -y * log(h(x)) - (1-y)log(1-h(x))
         //(-1/m) Sum(Cost)
-        final int    NUM_EXAMPLES   = 1000; //M
+        final int    NUM_EXAMPLES   = 100; //M
         final int    NUM_PARAMS     = 2; //N
-        final double ALPHA          = .001;
-        final int    NUM_ITERATIONS = 100000;
-        //final int    PRINT_AT        = 10000;
+        final double ALPHA          = .00001;
+        final int    NUM_ITERATIONS = 100000000;
+        final int    PRINT_AT       = 1000000;
         Random random = new Random();
-        double w0 = 6.0;
+        double w0 = 10.0;
         double w1 = .5;
         double w2 = (1.0/3.0);
 
@@ -279,15 +276,8 @@ public class TestSimpleDescent {
         //initialize Independent Xi
         //Going to create test data y= .5(x1) + .33(x2) + 10
         for( int x=0;x<NUM_EXAMPLES;x++) {
-            double x1 = random.nextDouble();
-            double x2 = random.nextDouble();
-
-            if( random.nextBoolean() ) {
-                x1 *= -1.0;
-            }
-            if( random.nextBoolean() ) {
-                x2 *= -1.0;
-            }
+            double x1 = random.nextGaussian();
+            double x2 = random.nextGaussian();
 
             independent.set(x, 0, 1); //We always set this to 1 for the intercept
             independent.set(x, 1, x1);
@@ -298,11 +288,11 @@ public class TestSimpleDescent {
         for( int x=0;x<NUM_EXAMPLES;x++) {
             double val      = w0 +  (w1*independent.get(x,1)) + (w2*independent.get(x,2));
             double logitVal = logit( val );
-//            if( logitVal < 0.5 ) {
-//                logitVal = 0;
-//            }else {
-//                logitVal = 1;
-//            }
+            if( logitVal < 0.5 ) {
+                logitVal = 0;
+            }else {
+                logitVal = 1;
+            }
             dependent.set(x, logitVal );
         }
 
@@ -313,12 +303,16 @@ public class TestSimpleDescent {
         for( int x=0;x<NUM_PARAMS+1;x++) {
             thetas.set(x,1);
         }
+        thetas.set(0,11.497702 );
+        thetas.set(1,0.0799 );
+        thetas.set(2,0.062471 );
+        //11.497702 0.0799 0.062471
 
         for( int x=0;x<NUM_ITERATIONS;x++) {
             thetas = logisticDescent( ALPHA, thetas, independent, dependent );
-//            if( x%PRINT_AT == 0) {
-//                System.out.println(thetas);
-//            }
+            if( x%PRINT_AT == 0) {
+                System.out.println(thetas);
+            }
         }
 
         //It seems like if we don't regularize to Zero mean, then the learning rate has to go way up or it goes off the
@@ -330,8 +324,169 @@ public class TestSimpleDescent {
         assertEquals(w2,thetas.get(2), EPSILON);
     }
 
-    public static double logit( double val ) {
-        return( 1.0 / (1.0 + Math.exp(-val)));
+    @Test
+    public void testLogisticDescentMultipleTwo() {
+        //Cost function: -y * log(h(x)) - (1-y)log(1-h(x))
+        //(-1/m) Sum(Cost)
+        final int    NUM_EXAMPLES   = 100; //M
+        final int    NUM_PARAMS     = 2; //N
+        final double ALPHA          = .00001;
+        final int    NUM_ITERATIONS = 100000000;
+        final int    PRINT_AT       = 1000000;
+        AtomicInteger cntr = new AtomicInteger();
+
+        //These are the weights for linear regression (Theta or Beta depending on your preference)
+        DoubleMatrix1D thetas           = new DenseDoubleMatrix1D(NUM_PARAMS+1);
+
+        //rows,columns
+        //Xi
+        //These are the example data, i(down the column) is instance, j is each feature (across the row)
+        DoubleMatrix2D independent      = new DenseDoubleMatrix2D(NUM_EXAMPLES,NUM_PARAMS+1);
+
+        //Yi
+        //These are the results of the example linear equation.
+        DoubleMatrix1D dependent        = new DenseDoubleMatrix1D(NUM_EXAMPLES);
+
+
+        add(independent,dependent,cntr,34.62365962451697,78.0246928153624,0);
+        add(independent,dependent,cntr,30.28671076822607,43.89499752400101,0);
+        add(independent,dependent,cntr,35.84740876993872,72.90219802708364,0);
+        add(independent,dependent,cntr,60.18259938620976,86.30855209546826,1);
+        add(independent,dependent,cntr,79.0327360507101,75.3443764369103,1);
+        add(independent,dependent,cntr,45.08327747668339,56.3163717815305,0);
+        add(independent,dependent,cntr,61.10666453684766,96.51142588489624,1);
+        add(independent,dependent,cntr,75.02474556738889,46.55401354116538,1);
+        add(independent,dependent,cntr,76.09878670226257,87.42056971926803,1);
+        add(independent,dependent,cntr,84.43281996120035,43.53339331072109,1);
+        add(independent,dependent,cntr,95.86155507093572,38.22527805795094,0);
+        add(independent,dependent,cntr,75.01365838958247,30.60326323428011,0);
+        add(independent,dependent,cntr,82.30705337399482,76.48196330235604,1);
+        add(independent,dependent,cntr,69.36458875970939,97.71869196188608,1);
+        add(independent,dependent,cntr,39.53833914367223,76.03681085115882,0);
+        add(independent,dependent,cntr,53.9710521485623,89.20735013750205,1);
+        add(independent,dependent,cntr,69.07014406283025,52.74046973016765,1);
+        add(independent,dependent,cntr,67.94685547711617,46.67857410673128,0);
+        add(independent,dependent,cntr,70.66150955499435,92.92713789364831,1);
+        add(independent,dependent,cntr,76.97878372747498,47.57596364975532,1);
+        add(independent,dependent,cntr,67.37202754570876,42.83843832029179,0);
+        add(independent,dependent,cntr,89.67677575072079,65.79936592745237,1);
+        add(independent,dependent,cntr,50.534788289883,48.85581152764205,0);
+        add(independent,dependent,cntr,34.21206097786789,44.20952859866288,0);
+        add(independent,dependent,cntr,77.9240914545704,68.9723599933059,1);
+        add(independent,dependent,cntr,62.27101367004632,69.95445795447587,1);
+        add(independent,dependent,cntr,80.1901807509566,44.82162893218353,1);
+        add(independent,dependent,cntr,93.114388797442,38.80067033713209,0);
+        add(independent,dependent,cntr,61.83020602312595,50.25610789244621,0);
+        add(independent,dependent,cntr,38.78580379679423,64.99568095539578,0);
+        add(independent,dependent,cntr,61.379289447425,72.80788731317097,1);
+        add(independent,dependent,cntr,85.40451939411645,57.05198397627122,1);
+        add(independent,dependent,cntr,52.10797973193984,63.12762376881715,0);
+        add(independent,dependent,cntr,52.04540476831827,69.43286012045222,1);
+        add(independent,dependent,cntr,40.23689373545111,71.16774802184875,0);
+        add(independent,dependent,cntr,54.63510555424817,52.21388588061123,0);
+        add(independent,dependent,cntr,33.91550010906887,98.86943574220611,0);
+        add(independent,dependent,cntr,64.17698887494485,80.90806058670817,1);
+        add(independent,dependent,cntr,74.78925295941542,41.57341522824434,0);
+        add(independent,dependent,cntr,34.1836400264419,75.2377203360134,0);
+        add(independent,dependent,cntr,83.90239366249155,56.30804621605327,1);
+        add(independent,dependent,cntr,51.54772026906181,46.85629026349976,0);
+        add(independent,dependent,cntr,94.44336776917852,65.56892160559052,1);
+        add(independent,dependent,cntr,82.36875375713919,40.61825515970618,0);
+        add(independent,dependent,cntr,51.04775177128865,45.82270145776001,0);
+        add(independent,dependent,cntr,62.22267576120188,52.06099194836679,0);
+        add(independent,dependent,cntr,77.19303492601364,70.45820000180959,1);
+        add(independent,dependent,cntr,97.77159928000232,86.7278223300282,1);
+        add(independent,dependent,cntr,62.07306379667647,96.76882412413983,1);
+        add(independent,dependent,cntr,91.56497449807442,88.69629254546599,1);
+        add(independent,dependent,cntr,79.94481794066932,74.16311935043758,1);
+        add(independent,dependent,cntr,99.2725269292572,60.99903099844988,1);
+        add(independent,dependent,cntr,90.54671411399852,43.39060180650027,1);
+        add(independent,dependent,cntr,34.52451385320009,60.39634245837173,0);
+        add(independent,dependent,cntr,50.2864961189907,49.80453881323059,0);
+        add(independent,dependent,cntr,49.58667721632031,59.80895099453265,0);
+        add(independent,dependent,cntr,97.64563396007767,68.86157272420604,1);
+        add(independent,dependent,cntr,32.57720016809309,95.59854761387875,0);
+        add(independent,dependent,cntr,74.24869136721598,69.82457122657193,1);
+        add(independent,dependent,cntr,71.79646205863379,78.45356224515052,1);
+        add(independent,dependent,cntr,75.3956114656803,85.75993667331619,1);
+        add(independent,dependent,cntr,35.28611281526193,47.02051394723416,0);
+        add(independent,dependent,cntr,56.25381749711624,39.26147251058019,0);
+        add(independent,dependent,cntr,30.05882244669796,49.59297386723685,0);
+        add(independent,dependent,cntr,44.66826172480893,66.45008614558913,0);
+        add(independent,dependent,cntr,66.56089447242954,41.09209807936973,0);
+        add(independent,dependent,cntr,40.45755098375164,97.53518548909936,1);
+        add(independent,dependent,cntr,49.07256321908844,51.88321182073966,0);
+        add(independent,dependent,cntr,80.27957401466998,92.11606081344084,1);
+        add(independent,dependent,cntr,66.74671856944039,60.99139402740988,1);
+        add(independent,dependent,cntr,32.72283304060323,43.30717306430063,0);
+        add(independent,dependent,cntr,64.0393204150601,78.03168802018232,1);
+        add(independent,dependent,cntr,72.34649422579923,96.22759296761404,1);
+        add(independent,dependent,cntr,60.45788573918959,73.09499809758037,1);
+        add(independent,dependent,cntr,58.84095621726802,75.85844831279042,1);
+        add(independent,dependent,cntr,99.82785779692128,72.36925193383885,1);
+        add(independent,dependent,cntr,47.26426910848174,88.47586499559782,1);
+        add(independent,dependent,cntr,50.45815980285988,75.80985952982456,1);
+        add(independent,dependent,cntr,60.45555629271532,42.50840943572217,0);
+        add(independent,dependent,cntr,82.22666157785568,42.71987853716458,0);
+        add(independent,dependent,cntr,88.9138964166533,69.80378889835472,1);
+        add(independent,dependent,cntr,94.83450672430196,45.69430680250754,1);
+        add(independent,dependent,cntr,67.31925746917527,66.58935317747915,1);
+        add(independent,dependent,cntr,57.23870631569862,59.51428198012956,1);
+        add(independent,dependent,cntr,80.36675600171273,90.96014789746954,1);
+        add(independent,dependent,cntr,68.46852178591112,85.59430710452014,1);
+        add(independent,dependent,cntr,42.0754545384731,78.84478600148043,0);
+        add(independent,dependent,cntr,75.47770200533905,90.42453899753964,1);
+        add(independent,dependent,cntr,78.63542434898018,96.64742716885644,1);
+        add(independent,dependent,cntr,52.34800398794107,60.76950525602592,0);
+        add(independent,dependent,cntr,94.09433112516793,77.15910509073893,1);
+        add(independent,dependent,cntr,90.44855097096364,87.50879176484702,1);
+        add(independent,dependent,cntr,55.48216114069585,35.57070347228866,0);
+        add(independent,dependent,cntr,74.49269241843041,84.84513684930135,1);
+        add(independent,dependent,cntr,89.84580670720979,45.35828361091658,1);
+        add(independent,dependent,cntr,83.48916274498238,48.38028579728175,1);
+        add(independent,dependent,cntr,42.2617008099817,87.10385094025457,1);
+        add(independent,dependent,cntr,99.31500880510394,68.77540947206617,1);
+        add(independent,dependent,cntr,55.34001756003703,64.9319380069486,1);
+        add(independent,dependent,cntr,74.77589300092767,89.52981289513276,1);
+
+
+//        System.out.println(independent);
+//        System.out.println(dependent);
+
+        //Initialize Thetas to all 1.
+        for( int x=0;x<NUM_PARAMS+1;x++) {
+            thetas.set(x,1);
+        }
+        //-25.161333 0.206232 0.201472
+//        thetas.set(0,-25.161334);
+//        thetas.set(1,0.206232);
+//        thetas.set(2,0.201472);
+
+
+        for( int x=0;x<NUM_ITERATIONS;x++) {
+            thetas = logisticDescent( ALPHA, thetas, independent, dependent );
+            if( x%PRINT_AT == 0) {
+                System.out.println(thetas);
+            }
+        }
+
+        //It seems like if we don't regularize to Zero mean, then the learning rate has to go way up or it goes off the
+        //rails real quick.
+
+        //TODO: Not sure why this isn't what I put in...
+        assertEquals(-25.161334,thetas.get(0), EPSILON);
+        assertEquals(0.206232,thetas.get(1), EPSILON);
+        assertEquals(0.201472,thetas.get(2), EPSILON);
+    }
+
+    private static void add (DoubleMatrix2D independent, DoubleMatrix1D dependent, AtomicInteger cntr, double x1, double x2, int y) {
+        independent.set(cntr.get(), 0, 1); //We always set this to 1 for the intercept
+        independent.set(cntr.get(), 1, x1);
+        independent.set(cntr.get(), 2, x2 );
+
+        dependent.set(cntr.get(), y );
+
+        cntr.incrementAndGet();
     }
 
 
